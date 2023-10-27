53e1921728e3f4c9fb3872ea138455f79458972f
namo333 <dad688634@gmail.com>
2023-10-27 18:59:19 +0300

    add base

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        // "bg-dark": "var(--bg-dark)",
+        // "bg-light": "var(--bg-light)",
+        // "blue-12": "var(--blue-12)",
+        // "blue-16": "var(--blue-16)",
+        // "collection-1-main-text": "var(--collection-1-main-text)",
+      },
+    },
+  },
+  plugins: [],
+};
