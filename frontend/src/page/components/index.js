0ae70485b17d0b5309136eb5418b3f087f84f1f9
namo333 <dad688634@gmail.com>
2023-11-01 10:57:08 +0300

    fix all

--- frontend/src/page/components/index.js
+++ frontend/src/page/components/index.js
@@ -2,12 +2,10 @@ import HeroHome from './HeroHome';
 import Info from './Info';
 import Company from './Company';
 import Reviews from './Reviews';
-import ReviewsSlider from './ReviewsSlider';
 
 export {
     HeroHome,
     Info,
     Company,
     Reviews,
-    ReviewsSlider,
 }
